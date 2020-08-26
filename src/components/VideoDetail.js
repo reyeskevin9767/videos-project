1fb1128cb2b5273a90d479a999170a9bb9be6021
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-26 02:26:58 -0400

    add video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,9 +6,13 @@ const VideoDetail = ({ video }) => {
   }
 
   const { title, description } = video.snippet;
-  
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} allowFullScreen />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{title}</h4>
         <p>{description}</p>
