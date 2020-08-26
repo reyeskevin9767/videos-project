de6f05932765776d8ab2007bcd35ae5dac8dd2e0
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-26 02:41:46 -0400

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,11 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  // Manually call onTermSumbit when application first loads
+  componentDidMount() {
+    this.onTermSubmit('react js');
+  }
+
   // Send a request to api
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -16,7 +21,10 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   // Get info about video from videoItem
@@ -29,11 +37,12 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
+        <div className="ui stackable grid ">
           <div className="row">
             <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
             </div>
+
             <div className="five wide column">
               <VideoList
                 onVideoSelect={this.onVideoSelect}
