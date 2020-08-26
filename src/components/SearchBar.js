756c3dd7e4a85c35a88ec87fdc49af6fab46ff3a
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-26 00:30:52 -0400

    add axios

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  // Set term from event handler
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  // Prevent broswer from submiting automatically
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Call callback function from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment" style={{ marginTop: '10px' }}>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field"></div>
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            // Event Handler
+            onChange={this.onInputChange}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
